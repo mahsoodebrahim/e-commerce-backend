a8e89c15715f46325592f3c2ac93b0d7cca9ebd3
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2023-05-17 21:44:53 -0700

    Add Register and Login controllers

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,59 @@
-exports.register = (req, res, next) => {
-  res.send("register");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
+
+exports.register = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.send("Did not provide name or email or password");
+  }
+
+  const isExistingUser = await User.findOne({ email: email });
+  if (isExistingUser) {
+    return res.send("User already exists");
+  }
+
+  try {
+    let newUser = { name, email, password };
+    newUser = await User.create(newUser);
+
+    res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "User created successfully", user: newUser });
+  } catch (error) {
+    if (error.code === 11000) {
+      // 11000: This is the error code associated with a duplicate key error in MongoDB.
+      res.json({ msg: "User with that email already exists" });
+    } else {
+      res.json({ msg: "Unable to register user", error: error });
+    }
+  }
 };
 
-exports.login = (req, res, next) => {
-  res.send("login");
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.send("Did not provide email or password");
+  }
+
+  const user = await User.findOne({ email: email });
+
+  if (user.password !== password) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ msg: "Invalid email or password" });
+  }
+
+  const jwtData = { role: user.role, email };
+  const token = jwt.sign(jwtData, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Successfully logged in", token: token });
 };
 
 exports.logout = (req, res, next) => {

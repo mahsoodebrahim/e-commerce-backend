2d90169baf5c6a75b231e6b2e4d0ab03f2cf4c62
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2023-05-17 21:44:06 -0700

    Connect to mongodb via mongoose

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const authRoutes = require("./src/routes/authRoutes");
 
 const app = express();
 
+// Middleware
+app.use(express.json()); // for parsing application/json
+
+// Routing
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res, next) => {
   res.status(200).json({ msg: "Hello World" });
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(3000, async () => {
+  await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING);
+  console.log(`Listening on port ${port}`);
 });
